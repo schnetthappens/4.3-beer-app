05acd7c463e169587bc11c6f30a8f1aa266751b2
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-27 20:56:23 -0400

    Make collapsed sections slide down when clicked

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@
   'use strict';
 
   $(document).ready(function(){
-    $('.section-category').hide();
+    $('.section-category').hide(); //hide contents of list on page load
   });
 
-  $(".section-heading").click(function() {
+  $('.section-heading').click(function() {
     $('.section-heading').siblings('.section-categories').slideUp("slow", function(){});
     $(this).next().slideDown("slow", function(){});
   });
